4353194cf55825768a1cb7a83ebabef896771d8f
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-12 16:23:50 +0300

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -5,20 +5,24 @@ class Product:
     count: int
     category: int
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, id, title, price, count, category):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.count = count
+        self.category = category
 
     def __bool__(self):
         """
         Проверяет есть ли товар в наличии
         """
-        pass
+        return bool(self.count > 0)
 
     def __len__(self):
         """
         Возвращает количество товара на складе
         """
-        pass
+        return self.count
 
 
 class Category:
@@ -27,17 +31,32 @@ class Category:
     description: str
     products: list
 
-    def __init__(self):
-        pass
+    def __init__(self, id, title, description, products):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.products = products
 
     def __bool__(self):
         """
-        Проверяет есть ли товар в категории
+        Проверяет есть ли товары в категории
         """
-        pass
+        return bool(len(self.products))
+        # return bool(any([True for x in self.products if x.title in x])) # есть ли конкретный товар в категории
 
     def __len__(self):
         """
         Возвращает количество наименований товаров, у которых есть наличие на складе
         """
-        pass
+        return sum([1 for product in self.products if product.count > 0])
+
+
+if __name__ == '__main__':
+    p1 = Product(1, 'апельсин', 34.5, 15, 1)
+    p2 = Product(2, 'мандарин', 47.5, 26, 1)
+    p3 = Product(2, 'банан', 25.3, 0, 1)
+    assert bool(p3) is False  # товара р3 нет в наличии на складе
+    assert len(p2) == 26  # товара p2 на складе 26 штук
+    P = Category(1, 'фрукты', 'вкусные', [p1, p2, p3])
+    assert bool(P) is True  # есть товары в данной категории
+    assert len(P) == 2  # количество различных товаров, из данной категории, которые есть на складе в данный момент
